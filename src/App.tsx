fb71e9d65196d9f346568b835a36ab2fc6108029
ebos4mum-star <ebos4mum@gmail.com>
2026-02-20 18:32:47 -0800

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/rain-wise-engine">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/terms" element={<Terms />} />
